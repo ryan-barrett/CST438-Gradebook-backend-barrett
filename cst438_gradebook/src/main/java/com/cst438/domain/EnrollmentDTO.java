4c3540c0300fe8109d79a81ed551ac7f193110b7
Ryan Barrett <ryan.barrett@spookyhouse.org>
2021-09-26 17:16:15 -0700

    implement EnrollmentController

--- cst438_gradebook/src/main/java/com/cst438/domain/EnrollmentDTO.java
+++ cst438_gradebook/src/main/java/com/cst438/domain/EnrollmentDTO.java
@@ -1,17 +1,25 @@
 package com.cst438.domain;
 
 public class EnrollmentDTO {
-	public int id;
-	public String studentEmail;
-	public String studentName;
-	public int course_id;
-	
-	public EnrollmentDTO() { } ;
-	
-	public EnrollmentDTO(String studentEmail, String studentName, int course_id) {
-		this.id = 0;
-		this.studentEmail=studentEmail;
-		this.studentName=studentName;
-		this.course_id = course_id;
-	}
+    public int id;
+    public String studentEmail;
+    public String studentName;
+    public int course_id;
+
+    public EnrollmentDTO() {
+    }
+
+    public EnrollmentDTO(Enrollment enrollment) {
+        this.id = enrollment.getId();
+        this.studentName = enrollment.getStudentName();
+        this.studentEmail = enrollment.getStudentEmail();
+        this.course_id = enrollment.getCourse().getCourse_id();
+    }
+
+    public EnrollmentDTO(String studentEmail, String studentName, int course_id) {
+        this.id = 0;
+        this.studentEmail = studentEmail;
+        this.studentName = studentName;
+        this.course_id = course_id;
+    }
 }
